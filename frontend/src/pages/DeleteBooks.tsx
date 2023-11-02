afc143615a279af844e01993d0d94b49c10366c7
Russel Kurt Nolasco <kurtlonolasco@gmail.com>
2023-11-02 18:02:45 +0800

    delete and update

--- frontend/src/pages/DeleteBooks.tsx
+++ frontend/src/pages/DeleteBooks.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
 
 const DeleteBooks = () => {
+
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const handleDeleteBooks = () => {
+    setLoading(true)
+    axios
+      .delete(`http://localhost:5555/books/${id}`)
+      .then(() => {
+        setLoading(false)
+        navigate('/')
+      })
+      .catch((error) => {
+        setLoading(false)
+        console.log(error)
+      })
+  }
+
   return (
-    <div>DeleteBooks</div>
+    <section>
+      <div>
+        <BackButton />
+        <h1>Delete Book</h1>
+      </div>
+      {loading ? <Spinner /> : ''}
+      <div>
+        <h3>Are you sure you want to delete this?</h3>
+        <button onClick={handleDeleteBooks}>Delete</button>
+      </div>
+    </section>
   )
 }
 
--- frontend/src/pages/UpdateBooks.tsx
+++ frontend/src/pages/UpdateBooks.tsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import BackButton from "../components/BackButton"
+import Spinner from "../components/Spinner"
 
 const UpdateBooks = () => {
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [publishYear, setPublishYear] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const navigate = useNavigate()
+  const { id } = useParams()
+
+  useEffect(() => {
+    setLoading(true)
+    axios.get(`http://localhost:5555/books/${id}`)
+      .then((response) => {
+        setTitle(response.data.title)
+        setAuthor(response.data.author)
+        setPublishYear(response.data.publishYear)
+        setLoading(false)
+      })
+      .catch((error) => {
+        setLoading(false)
+        console.log(error)
+      })
+  }, [])
+
+  const handleUpdateBook = () => {
+    const formData = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true)
+    axios
+      .put(`http://localhost:5555/books/${id}`, formData)
+      .then(() => {
+        setLoading(false)
+        navigate('/')
+      })
+      .catch((error) => {
+        console.log(error)
+        console.log('failed!')
+        setLoading(false)
+      })
+  }
+
   return (
-    <div>UpdateBooks</div>
+    <section>
+      <div>
+        <BackButton />
+        <h1>Update Book</h1>
+      </div>
+      {loading ? ( <Spinner /> ) : ('')}
+      <div>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title"/>
+        <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} placeholder="Author"/>
+        <input type="number" value={publishYear} onChange={(e) => setPublishYear(e.target.value)} placeholder="Publish Year"/>
+        <button type="submit" onClick={handleUpdateBook}>Update</button>
+      </div>
+    </section>
   )
 }
 
