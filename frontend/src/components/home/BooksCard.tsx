c5eee782e2375c48dee356740d147947e7b8d070
Russel Kurt Nolasco <kurtlonolasco@gmail.com>
2023-11-18 11:18:36 +0800

    added

--- frontend/src/components/home/BooksCard.tsx
+++ frontend/src/components/home/BooksCard.tsx
@@ -1,9 +1,12 @@
 import { Book } from '../../types'
+import SingleBookCard from './SingleBookCard'
 
 const BooksCard = ({ books }: { books: Book[]}) => {
   return (
     <div>
-        
+        {books.map((item) =>(
+          <SingleBookCard key={item._id} book={item}/>
+        ))}
     </div>
   )
 }
